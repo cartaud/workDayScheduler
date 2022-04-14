3a84ac7aada2b29e2e881ccc0b5daf92abbd7d36
Chadd <chadd_artaud@cox.net>
2022-04-14 14:54:13 -0700

    improved README

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 let containerEl = $('.container');
 let currentDayEl = $('#currentDay');
-currentDayEl.text(moment().format('dddd, MMMM Do')); //format example --> Wednesday, April 13th 
+currentDayEl.text(moment().format('dddd, MMMM Do')); //displays current date on page in format --> Wednesday, April 13th 
 
 let currentHourMinute = moment().format(); //format example --> 2022-04-14T13:07:00-07:00
 let currentHour = currentHourMinute[11]+currentHourMinute[12]; //returns just the hour 01-24
-containerEl.on('click', 'button', saveText)
-//Can you set an elements value to a local storage? ex --><textarea value="localStorage.getItem()"
+
+containerEl.on('click', 'button', saveText) //targets all buttons that are within the containerEl tree
+
+//This function creates all the time blocks for the calender and appends them to page
 function createCalender() {
     for (let i=9; i<24;i++) {
         let timeBlockEl = $('<div>');
@@ -15,11 +17,11 @@ function createCalender() {
         let rowEl = $('<div>');
         rowEl.addClass('row');
         let inputTextEl = $('<textarea>');
-        inputTextEl.text(localStorage.getItem(`toDo${i}`) || '');
+        inputTextEl.text(localStorage.getItem(`toDo${i}`) || ''); //creates a local storage for each textarea
         let lockButtonEl = $('<button>');
-        lockButtonEl.append('<i class="fa-solid fa-floppy-disk"></i>');
+        lockButtonEl.append('<i class="fa-solid fa-floppy-disk"></i>');//adds save icon to button
         lockButtonEl.addClass('saveBtn');
-        lockButtonEl.attr('id', `${i}`)
+        lockButtonEl.attr('id', `${i}`) //each button has different id (used to match button to textarea for saving text)
         containerEl.append(timeBlockEl);
         timeBlockEl.append(hourContainerEl);
         timeBlockEl.append(rowEl)
@@ -30,34 +32,36 @@ function createCalender() {
     }
 }
 
-function setHour(hour, hourEl){
-    if (hour < 12) {
+function setHour(hour, hourEl){ //sets the displayed hour for each time block
+    if (hour < 12) { //when hour is less than 12, we append am to hour
         hourEl.text(hour + 'am');
     }
-    else if (hour == 12) {
+    else if (hour == 12) { //when hour equals 12, we append pm to hour
         hourEl.text(hour + 'pm');
     }
-    else {
+    else {  //when hour is greater than 12, we want to restart and show (1pm-11pm) so we subtract 12 from hour
         hourEl.text(hour-12 +'pm');
     }
 }
 
+//depending on the current hour, the background of textarea will change
 function setColor(i, inputEl) {
-    if (i < currentHour) {
+    if (i < currentHour) {//makes background grey for timeblocks that have past
         inputEl.addClass('past');
     }
-    else if (i==currentHour) {
+    else if (i==currentHour) {//makes the current timeblock background red
         inputEl.addClass('present');
     }
-     else {
+     else {//makes future timeblocks green
          inputEl.addClass('future')
      }
 }
-//I need to somehow get the position of the textbox in respect to the container element
-function saveText(e) {
+
+//saves the text of each individual textarea to local storage on button click
+function saveText() {
     for (let i=9; i<24;i++){
-        if ($(this).attr('id') == i) {
-            localStorage.setItem(`toDo${i}`, $(this).siblings().val());
+        if ($(this).attr('id') == i) { //matches the id of the button that was clicked to the specified local storage
+            localStorage.setItem(`toDo${i}`, $(this).siblings().val()); 
         }
     }
 }
