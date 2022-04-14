dcdd8c40d950a8bf3a3943c4530ea34b4c5f9192
Chadd <chadd_artaud@cox.net>
2022-04-14 11:38:18 -0700

    hour blocks colors change depending on current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
+let containerEl = $('.container');
 let currentDayEl = $('#currentDay');
 currentDayEl.text(moment().format('dddd, MMMM Do')); //format example --> Wednesday, April 13th 
 
-let containerEl = $('.container');
+let currentHourMinute = moment().format('LT');
+let currentHour = currentHourMinute[0]+currentHourMinute[1];
+
+let amOrPm = currentHourMinute[6]+currentHourMinute[7]
+if (amOrPm == 'PM' && currentHour!=12) {
+    currentHour = currentHour +12;
+}
+
 
 function createCalender() {
     for (let i=9; i<24;i++) {
@@ -9,7 +17,6 @@ function createCalender() {
         timeBlockEl.addClass('time-block');
         let hourContainerEl = $('<div>');
         hourContainerEl.addClass('hour');
-        hourContainerEl.text(i);
         let rowEl = $('<div>');
         rowEl.addClass('row');
         let inputTextEl = $('<input>');
@@ -20,7 +27,34 @@ function createCalender() {
         timeBlockEl.append(rowEl)
         rowEl.append(inputTextEl);
         rowEl.append(lockButtonEl);
+        setHour(i, hourContainerEl);
+        setColor(i, inputTextEl);
     }
 }
+
+function setHour(hour, hourEl){
+    if (hour < 12) {
+        hourEl.text(hour + 'am');
+    }
+    else if (hour == 12) {
+        hourEl.text(hour + 'pm');
+    }
+    else {
+        hourEl.text(hour-12 +'pm');
+    }
+}
+
+function setColor(i, inputEl) {
+    if (i < currentHour) {
+        inputEl.addClass('past');
+    }
+    else if (i==currentHour) {
+        inputEl.addClass('present');
+    }
+     else {
+         inputEl.addClass('future')
+     }
+}
+
 
 createCalender()
